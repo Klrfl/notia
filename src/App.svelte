29b76bc6e21a0d14ce7a5c7707fc4f0f38c97943
Efraim Munthe <efrayanglain@gmail.com>
2025-02-27 21:06:51 +0700

    feat: create note state

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,45 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg"
-  import viteLogo from "/vite.svg"
-  import Counter from "./lib/Counter.svelte"
+  let title = "Notia"
+  const notes = $state([
+    {
+      id: 1,
+      title: "a note",
+      content: "i would like to learn svelte.. like right now",
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      title: "another note",
+      content: "finish this markdown notetaking app",
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+  ])
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>{title}</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <ul>
+    {#each notes as note}
+      <li>
+        <h2>{note.title}</h2>
+        <p>{note.content}</p>
 
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+        <p>
+          Created at {Intl.DateTimeFormat("en-US", {
+            dateStyle: "long",
+            timeStyle: "medium",
+          }).format(note.createdAt)}
+        </p>
+        <p>
+          Last updated at {Intl.DateTimeFormat("en-US", {
+            dateStyle: "long",
+            timeStyle: "medium",
+          }).format(note.updatedAt)}
+        </p>
+      </li>
+    {/each}
+  </ul>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
