67fe1c15e0cd75804bdcd92d1593ed21bab02580
Efraim Munthe <efrayanglain@gmail.com>
2025-03-10 09:54:32 +0700

    lint: setup linting

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from "globals"
+import js from "@eslint/js"
+import ts from "typescript-eslint"
+import svelte from "eslint-plugin-svelte"
+import svelteConfig from "./svelte.config.js"
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"],
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  }
+)
