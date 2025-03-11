3c942da8e0bb9b9e1f515e4fa15b4b316b980c7c
Efraim Munthe <efrayanglain@gmail.com>
2025-03-11 20:38:57 +0700

    ui: make edit note more responsive
    - add preview toggle for mobile

--- src/lib/NoteEdit.svelte
+++ src/lib/NoteEdit.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Pencil, Copy } from "lucide-svelte"
+  import { Pencil, Copy, Eye } from "lucide-svelte"
   import Button from "./ui/Button.svelte"
   import Input from "./ui/Input.svelte"
   import type { Note, NoteCategory } from "../types/"
@@ -25,18 +25,28 @@
 
     req.addEventListener("success", () => (categories = req.result))
   })
+
+  let isMobile = $state(false)
+  window.addEventListener(
+    "resize",
+    () => (isMobile = window.matchMedia("(max-width: 768px)").matches)
+  )
+  onMount(() => (isMobile = window.matchMedia("(max-width: 768px)").matches))
+
+  let textarea: HTMLTextAreaElement | undefined = $state()
+  let showPreview = $state(false)
 </script>
 
 <form
   method="dialog"
-  class="grid grid-cols-6 grid-flow-dense gap-4"
+  class="grid md:grid-cols-6 grid-flow-dense md:gap-4"
   onsubmit={() => {
     // you can't unwrap proxies natively
     // what is wrong with Svelte what the f
     editNote(JSON.parse(JSON.stringify(editedNote)))
   }}
 >
-  <fieldset class="flex flex-col gap-4 col-span-2">
+  <fieldset class="flex flex-col gap-4 col-span-full md:col-span-2">
     <label for="title">Title</label>
     <Input
       type="text"
@@ -76,27 +86,55 @@
       <p class="text-sm text-gray-600">Format your text in markdown.</p>
     </label>
 
-    <textarea
-      class="bg-gray-200/50 focus-visible:outline focus-visible:outline-gray-500 px-4 py-2 min-h-80"
-      id="content"
-      name="content"
-      bind:value={editedNote.content}
-      required
-    >
-    </textarea>
+    {#if isMobile}
+      <div class="flex">
+        <button
+          type="button"
+          onclick={() => (showPreview = false)}
+          disabled={!showPreview}
+          class="preview-toggle input"
+        >
+          <Pencil />
+          input
+        </button>
+
+        <button
+          type="button"
+          onclick={() => (showPreview = true)}
+          disabled={showPreview}
+          class="preview-toggle preview"
+        >
+          <Eye />
+          preview
+        </button>
+      </div>
+    {/if}
+
+    {#if !isMobile || !showPreview}
+      <textarea
+        bind:this={textarea}
+        class="bg-gray-200/50 focus-visible:outline focus-visible:outline-gray-500 p-4 md:min-h-80"
+        rows={textarea ? Math.round(textarea.clientHeight / 12) : 0}
+        id="content"
+        name="content"
+        bind:value={editedNote.content}
+        required
+      >
+      </textarea>
+    {/if}
   </fieldset>
 
-  <div class="preview bg-gray-100 p-4 col-span-4">
-    {#await marked.parse(editedNote.content.trim())}
-      <p>please wait</p>
-    {:then parsed}
-      {@html DOMPurify.sanitize(parsed)}
-    {:catch}
-      <p>something went wrong when rendering your note.</p>
-    {/await}
-  </div>
+  {#if !isMobile || showPreview}
+    <div class="preview bg-gray-100 p-4 md:col-span-4">
+      {#await marked.parse(editedNote.content.trim()) then parsed}
+        {@html DOMPurify.sanitize(parsed)}
+      {:catch}
+        <p>something went wrong when rendering your note.</p>
+      {/await}
+    </div>
+  {/if}
 
-  <p class="text-sm text-gray-600 order-last text-right col-span-4">
+  <p class="text-sm text-gray-600 order-last text-right md:col-span-4">
     <span class="block">
       {Intl.DateTimeFormat("en-US", {
         dateStyle: "long",
@@ -132,4 +170,19 @@
 
 <style>
   @reference "../app.css";
+
+  .preview-toggle {
+    border-top: transparent;
+    border-bottom: transparent;
+    @apply flex gap-4 px-4 py-2 cursor-pointer bg-gray-200/50 border-2 border-t-slate-200 not-disabled:bg-gray-300 not-disabled:text-gray-600 disabled:cursor-not-allowed;
+  }
+
+  .preview-toggle.input {
+    @apply border-l-slate-200;
+    border-right: transparent;
+  }
+
+  .preview-toggle.preview {
+    @apply border-l-slate-200 border-r-slate-200;
+  }
 </style>
