66c62fd2a0592d2f12bf335b6acd9b8d603eeecd
Efraim Munthe <efrayanglain@gmail.com>
2025-03-13 22:45:32 +0700

    feat: setup svelte routing and import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite"
+import { fileURLToPath, URL } from "node:url"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
